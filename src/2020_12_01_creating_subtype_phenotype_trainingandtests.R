ccd80a5d4ad46c8f30eb00e819464789c5c105b2
Jolien Rietkerk <jolien.rietkerk@helmholtz-muenchen.de>
2020-12-01 17:26:22 +0100

    no message

--- src/2020_12_01_creating_subtype_phenotype_trainingandtests.R
+++ src/2020_12_01_creating_subtype_phenotype_trainingandtests.R
@@ -1,26 +1,38 @@
-library(tidyverse)
-library(nycflights13)
+library(dplyr)
 
-#data <- read.table(pheontype file)
+
+path = "~/Documents/My Data/BRAZIL/Elections/"
+out.file<-""
+file.names <- dir(path, pattern =".txt")
+for(i in 1:length(file.names)){
+  file <- read.table(file.names[i],header=TRUE, sep=";", stringsAsFactors=FALSE)
+  out.file <- rbind(out.file, file)
+}
+ write.table(out.file, file = "cand_Brazil.txt",sep=";", 
+             row.names = FALSE, qmethod = "double",fileEncoding="windows-1252")
+             
+             
+
+
+data <- read.table("/projects/biobank/release2/pheno/MDD2.nowithdrawn.phen")
 
 data <- mutate(data, setnumber=rep_len(c(1:10),nrow(data)))
 
-for (i in 1:10) {
-  assign(paste("training", i, sep=""), filter(data, setnumber!= i)[,-ncol(data)])
-  assign(paste("test",i, sep=""), filter(data, setnumber== i)[,-ncol(data)])
-}
+out_base <- "~/data/"
+name_base <- "MDD2.nowithdrawn."
 
 
 for (i in 1:10) {
   training <- data.frame(filter(data,setnumber != i)[,-ncol(data)])
-  trainfile <- file.path(paste0("~/Desktop/training", i , ".txt"))
+  trainfile <- file.path(paste0(out_base,name_base,"training", i , ".phen"))
   write.table(training, file = trainfile, sep = " ", row.names = FALSE, col.names = FALSE,
               quote = FALSE, append = FALSE)
-  
+
   test <-  data.frame(filter(data,setnumber == i)[,-ncol(data)])
-  testfile <- file.path(paste0("~/Desktop/test", i ,".txt"))
+  testfile <- file.path(paste0(out_base,name_base,"test", i , ".phen"))
   write.table(test,file = testfile , sep = " ", row.names = FALSE, col.names = FALSE,
               quote = FALSE, append = FALSE)
 }
+
 
 
