8d09c439fa714dbfb378d752bbb99492fcacef73
Jolien Rietkerk <jolien.rietkerk@helmholtz-muenchen.de>
2020-12-07 14:10:23 +0100

    no message

--- src/2020_12_01_creating_subtype_phenotype_trainingandtests.R
+++ src/2020_12_01_creating_subtype_phenotype_trainingandtests.R
@@ -1,20 +1,10 @@
 library(dplyr)
 
 
-path = "~/Documents/My Data/BRAZIL/Elections/"
-out.file<-""
-file.names <- dir(path, pattern =".txt")
-for(i in 1:length(file.names)){
-  file <- read.table(file.names[i],header=TRUE, sep=";", stringsAsFactors=FALSE)
-  out.file <- rbind(out.file, file)
-}
- write.table(out.file, file = "cand_Brazil.txt",sep=";", 
-             row.names = FALSE, qmethod = "double",fileEncoding="windows-1252")
-             
-             
+args = commandArgs(trailingOnly=TRUE)
 
 
-data <- read.table("/projects/biobank/release2/pheno/MDD2.nowithdrawn.phen")
+data <- read.table(args)
 
 data <- mutate(data, setnumber=rep_len(c(1:10),nrow(data)))
 
